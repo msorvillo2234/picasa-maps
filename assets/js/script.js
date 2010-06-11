9e86c34d996b9bc5ca0b7c29a03c5be7ffa46281
Mike Sorvillo <mikesorvillo@gmail.com>
2010-06-10 22:27:39 -0400

    markers of picasa albums are now displayed on a map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,71 @@
-window.onload = initMap;
+//xx TODO - wrap Album into class & get pictures
+//xx TODO - set center dynamically
+//xx TODO - figure out JSON problem with Geocoder API
 
-var map;
-var geocoder;
 
-function initMap()
-{
-	map = new GMap2(document.getElementById("map"));
-	map.setUIToDefault();
+(function($) {
+	var map, geocoder, albums = [];
+	
+	$(document).ready(function(){
+		initMap();
+		getAlbums();
 
-	geocoder = new GClientGeocoder();
-  	geocoder.getLatLng("New York, NY", onAddressFound);
+	});	
+	
+	function initMap()
+	{
+		geocoder = new GClientGeocoder();
+		map = new GMap2($("div#map") .get(0));
+		map.setCenter(new GLatLng(30, -55), 3);
+		map.setUIToDefault();
+	}
+	
+	function getAlbums(){
+		$.getJSON('http://picasaweb.google.com/data/feed/api/user/mikesorvillo?alt=json', function(data) {
+			albums = data.feed.entry;
+			iterate(0);
+			
+			/*for(var i = 0; i < albums.length; i++){
+				console.log(i + " - " + albums[i].gphoto$location.$t);
+				$.getJSON("http://maps.google.com/maps/api/geocode/json?address=" + albums[i].gphoto$location.$t + "&sensor=false", function(data){
+					alert("HELLO")
+				});
+				
+				geocoder.getLatLng(albums[i].gphoto$location.$t, onAddressFound);
+			} */ 	
+		})			
+	}
+	
+	//using setTimeout for recursion. god, this is so hacky.
+	function iterate(count){
+		geocoder.getLatLng(albums[count].gphoto$location.$t, onAddressFound);
+		count++;
+		
+		if(count < albums.length){
+			setTimeout(function(){iterate(count)}, 200);
+		}
+	}
+	
+	function onAddressFound(point){
+		if (!point) {
+	    } else {
+	      	var marker = new GMarker(point);
+	      	map.addOverlay(marker);
+		  	GEvent.addListener(marker, "click", showPics);
+	    }
+	}
+	
+	function showPics(){
+		//console.log("showing pictures for...")
+	}
+	
+	function Album(id, loc){
+		this.albumId = id;
+		this.location = loc;
+	}
+	
+})(jQuery);
 
-	//populateMarkers(100);
-}
 
-function populateMarkers(n) {
-  for (var i = 0; i < n; ++i) {
-	var marker = new GMarker(getRandomPoint());
-	map.addOverlay(marker);
-  	GEvent.addListener(marker, "click", showInfoWindow);
-  }
-}
 
-function getRandomPoint() {
-  var lat = 48.25 + (Math.random() - 0.5)*14.5;
-  var lng = 11.00 + (Math.random() - 0.5)*36.0;
-  return new GLatLng(Math.round(lat*10)/10, Math.round(lng*10)/10);
-}
 
-function onAddressFound(latlng){
-	if (!latlng) {
-		alert(address + " not found");
-    } else {
-      	map.setCenter(latlng, 15);
-      	var marker = new GMarker(latlng);
-      	map.addOverlay(marker);
-	  	GEvent.addListener(marker, "click", showInfoWindow);
-    }
-}
-
-function showInfoWindow(latlng){
-	map.openInfoWindow(map.getCenter(), document.createTextNode("Hello, test!"));
-}
